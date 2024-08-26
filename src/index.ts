c880e3b31ffc6230c06dd733e80321e115226a35
TomiHenriksson8 <tomihen01@gmail.com>
2024-08-26 13:39:54 +0300

    change app to listen 0.0.0.0 for docker

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import app from './app';
 import mongoConnect from './utils/db';
 
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
 (async () => {
   try {
     await mongoConnect();
-    app.listen(port, () => {
-      console.log(`Listening: http://localhost:${port}`);
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Listening: http://0.0.0.0:${port}`);
     });
   } catch (error) {
     console.log('Server error', (error as Error).message);
